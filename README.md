357c3c61fae931d999f2280d6ce2a13b8c7e1ae2
Anime Nyan <tellm3pls@gmail.com>
2021-04-17 18:56:53 +1000

    Add installation instructions to readme and fix end of string search with better function .endswith()

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # UEShaderScript
 
-##Installation
+## Installation
+So download both UEShaderScript.py and AddUEShaderScript.py and open Blender. Once you are in Blender Edit > Preferences > Add-ons > Press on the Install... button
 
-
-##Usage
+## Usage
--- UEShaderScript.py
+++ UEShaderScript.py
@@ -24,9 +24,9 @@ bl_info = {
     "location": "TO BE ADDED",
     "description": "Adds different preset shader maps for Unreal Engine games Dead By Daylight and Home Sweet Home Survive characters that have already been imported into blender",
     "warning": "",
-    "wiki_url": "TO BE ADDED",
+    "wiki_url": "https://github.com/AnimNyan/UEShaderScript/wiki",
     "category": "Material",
-    "tracker_url": "TO BE ADDED"
+    "tracker_url": "https://github.com/AnimNyan/UEShaderScript"
 }
 
 """
@@ -496,7 +496,7 @@ def roman_noodles_basic_shader_map(material, props_txt_path, mytool):
             #check what the last two/three characters are of the id
             #and look for the specific ids we are interested in
             #identifier
-            if "_BC" == tex_id[-3:]:
+            if tex_id.endswith("_BC"):
                 bc_node = new_node('ShaderNodeTexImage')
                 bc_node.location = (-300,450) #x,y
                 bc_node.image = bpy.data.images.load(complete_path)
@@ -504,21 +504,21 @@ def roman_noodles_basic_shader_map(material, props_txt_path, mytool):
                 link(bc_node.outputs[0], rgbbw_node.inputs[0])
                 bc_node.interpolation = "Cubic"
                 
-            elif "_ORM" in tex_id[-4:]:
+            elif tex_id.endswith("_ORM"):
                 orm_node = new_node('ShaderNodeTexImage')
                 orm_node.location = (-750, 300) #x,y
                 orm_node.image = bpy.data.images.load(complete_path)
                 link(orm_node.outputs[0], srgb_node.inputs[0])
                 orm_node.image.colorspace_settings.name = "Non-Color"
                 
-            elif "_N" in tex_id[-2:]:
+            elif tex_id.endswith("_N"):
                 n_node = new_node('ShaderNodeTexImage')
                 n_node.location = (-800,-200) #x,y
                 n_node.image = bpy.data.images.load(complete_path)
                 link(n_node.outputs[0], normap_node.inputs[1])
                 n_node.image.colorspace_settings.name = "Non-Color"
                 
-            elif "_M" in tex_id[-2:]:
+            elif tex_id.endswith("_M"):
                 #add ramp node to connect to M node and control
                 #alpha if alpha transparency is required
                 ramp_node_2 = new_node("ShaderNodeValToRGB")
@@ -560,182 +560,6 @@ def create_one_hair_shader_map(context, material, props_txt_path, mytool):
 
 
 def yanima_hair_shader_map(material, props_txt_path, mytool):
-    #store new link function to variable
-    link = material.node_tree.links.new
-    
-    #store new node function to variable
-    new_node = material.node_tree.nodes.new
-
-    #start adding all nodes and respective links to shader map
-    #--------------add everything except image texture nodes
-    srgb_node = new_node("ShaderNodeSeparateRGB")
-    srgb_node.location = (-150,150) # x,y
-    link(srgb_node.outputs[2], principled_node.inputs["Metallic"])
-
-    ramp_node_1 = new_node("ShaderNodeValToRGB")
-    ramp_node_1.location = (-450,50) # x,y
-    ramp_node_1.color_ramp.elements[1].position = 0.209
-    link(ramp_node_1.outputs[0], principled_node.inputs["Roughness"])
-    
-    bump_node = new_node("ShaderNodeBump")
-    bump_node.location = (-200,-200) # x,y
-    bump_node.inputs[0].default_value = 0.175
-    link(bump_node.outputs[0], principled_node.inputs["Normal"])
-    
-    rgbbw_node = new_node("ShaderNodeRGBToBW")
-    rgbbw_node.location = (-150,0) # x,y
-    link(rgbbw_node.outputs[0], ramp_node_1.inputs["Fac"])
-    
-    normap_node = new_node(type="ShaderNodeNormalMap")
-    normap_node.location = (-400,-200) # x,y
-    link(normap_node.outputs[0], bump_node.inputs["Normal"])
-    
-    #only add skin related nodes for height map
-    #if material is for skin
-    if mytool.is_material_skin == True:
-        map_node = new_node(type="ShaderNodeMapping")
-        map_node.location = (-300,-450) # x,y
-        map_node.inputs[3].default_value[0] = 12
-        map_node.inputs[3].default_value[1] = 12
-        map_node.inputs[3].default_value[2] = 12
-        
-        textcoord_node = new_node(type="ShaderNodeTexCoord")
-        textcoord_node.location = (-500,-450) # x,y
-        link(textcoord_node.outputs[2], map_node.inputs[0])
-
-    
-    #open the propstxt file for the material and find the
-    #texture locations from it
-    #with just means open and close file
-    with open(props_txt_path, 'r') as f:
-        #read entire file to one string
-        data = f.read()
-        #find all matches through regex to the string Texture2D' with capture group 
-        #any character zero to unlimited times and ending with '
-        #also store capture groups into a list variable
-        match_list = re.findall("Texture2D\'(.*)\.", data)
-    
-    #only add image textures nodes if Include Image Textures
-    #in panel is true
-    if mytool.is_add_img_textures == True:
-        #---------------add image texture nodes    
-        #example loading image
-        #texImage.image = bpy.dat.images.load(
-        #"C:\Users\myName\Downloads\Textures\Downloaded\flooring5.jpg")
-        
-        #add height map if Add Skin Related Nodes is checked and 
-        #add Height Map Skin Texture is checked
-        #add the height map image texture and load the user defined
-        #height map image
-        
-        if mytool.is_add_img_textures == True and mytool.is_material_skin == True and mytool.is_add_height_map == True:
-            height_map_path = mytool.height_map_path
-            
-            height_map_node = new_node('ShaderNodeTexImage')
-            height_map_node.location = (-100,-450) #x,y
-            height_map_node.image = bpy.data.images.load(height_map_path)
-            link(height_map_node.outputs[0], bump_node.inputs[2])
-            link(map_node.outputs[0], height_map_node.inputs[0])
-            height_map_node.interpolation = "Cubic"
-        
-        
-        #use loop to go through all locations
-        #specified in props.txt file
-        #and create image texture nodes + 
-        #load all images for image textures
-        for tex_location in match_list:
-            
-            #fetch last 6 characters in path which will tell you what
-            #the current texture is in charge of e.g slice _BC off
-            #/Game/Characters/Slashers/Bear/Textures/Outfit01/T_BEHead01_BC
-            #longest id is _ORM or _BDE 4 characters 
-            tex_id = tex_location[-6:]
-            
-            #if the folder path is a relative path
-            #turn it into an absolute one
-            #as relative paths cause problems
-            #when trying to load an image
-            abs_export_folder_path = bpy.path.abspath(mytool.export_folder_path)
-            
-            # Returns user specified export game folder path
-            # with first character removed
-            # reason why is there would be double up of \/ when 
-            #concatenating strings
-            user_tex_folder = abs_export_folder_path[:-1]
-            
-            #replace forward slash with backslash reason why is
-            # when concatenating complete path looks like this
-            #if no replace path looks like e.g. C:\Nyan\Dwight Recolor\Game/Characters/Slashers/Bear/Textures/Outfit01/T_BEHead01_BC
-            #which is weird
-            #backslash is used to escape backslash character
-            tex_location = tex_location.replace("/","\\")
-            
-            #if the user selects the game folder instead of the
-            #parent folder, the first 5 characters of 
-            #the user input box: user_tex_folder will be "Game"
-            #so we remove "Game\" from the tex_location
-            #to avoid a double up
-            #this is extra redundancy so if the
-            #user chooses either the Game folder or
-            #the parent folder of the Game folder
-            #both options will work
-            if user_tex_folder[-4:] == "Game":
-                #backslash is used to escape backslash character
-                tex_location = tex_location.replace("\\Game","")
-     
-            #must string concatenate the user specified texture location path to 
-            #the texture location
-            #as the tex_location will only be 
-            #e.g /Game/Characters/Slashers/Bear/Textures/Outfit01/T_BEHead01_BC
-            #this does not provide a complete path to where the user exported
-            #the texture
-            #we need e.g. C:\Nyan\Dwight Recolor\Game\Characters
-            #\Slashers\Bear\Textures\Outfit01\T_BEHead01_BC
-            complete_path = user_tex_folder + tex_location + ".tga"
-
-            #If the texture is listed in the 
-            #props.txt file and it is one of the
-            #image textures we are interested in we will add the node
-            #and load the corresponding image
-                 
-            #check what the last two/three characters are of the id
-            #and look for the specific ids we are interested in
-            #identifier
-            if "_BC" == tex_id[-3:]:
-                bc_node = new_node('ShaderNodeTexImage')
-                bc_node.location = (-300,450) #x,y
-                bc_node.image = bpy.data.images.load(complete_path)
-                link(bc_node.outputs[0], principled_node.inputs[0])
-                link(bc_node.outputs[0], rgbbw_node.inputs[0])
-                bc_node.interpolation = "Cubic"
-                
-            elif "_ORM" in tex_id[-4:]:
-                orm_node = new_node('ShaderNodeTexImage')
-                orm_node.location = (-750, 300) #x,y
-                orm_node.image = bpy.data.images.load(complete_path)
-                link(orm_node.outputs[0], srgb_node.inputs[0])
-                orm_node.image.colorspace_settings.name = "Non-Color"
-                
-            elif "_N" in tex_id[-2:]:
-                n_node = new_node('ShaderNodeTexImage')
-                n_node.location = (-800,-200) #x,y
-                n_node.image = bpy.data.images.load(complete_path)
-                link(n_node.outputs[0], normap_node.inputs[1])
-                n_node.image.colorspace_settings.name = "Non-Color"
-                
-            elif "_M" in tex_id[-2:]:
-                #add ramp node to connect to M node and control
-                #alpha if alpha transparency is required
-                ramp_node_2 = new_node("ShaderNodeValToRGB")
-                ramp_node_2.location = (-750,20) # x,y
-                ramp_node_2.color_ramp.elements[1].position = 0.95
-                link(ramp_node_2.outputs[0], principled_node.inputs["Alpha"])
-                
-                m_node = new_node('ShaderNodeTexImage')
-                m_node.location = (-1100,20) #x,y
-                m_node.image = bpy.data.images.load(complete_path)
-                link(m_node.outputs[0], ramp_node_2.inputs[0])
-                material.blend_method = 'CLIP'
 
 
 classes = [PathProperties, UEShaderScript_PT_main_panel, UEShaderScript_OT_add_basic, UEShaderScript_OT_add_basic_all, UEShaderScript_OT_add_hair, UEShaderScript_OT_add_hair_all]
