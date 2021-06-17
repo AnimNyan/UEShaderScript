315919d841c2103e2fbf0986b4bf4b8d6e889595
Anime Nyan <tellm3pls@gmail.com>
2021-06-18 00:49:19 +1000

    add documentation on why we need a try and exception in the __init_ file + add feature to automatically update plugin on blender start

--- __init__.py
+++ __init__.py
@@ -44,14 +44,36 @@ Adds two panels one in the Shader Editor View to save different preset shader ma
 def register():
     save_shader_map.register()
     load_shader_map.register()
-    #import_current_or_default_json will not work when blender just
-    #starts the reason why is because you cannot get add on preferences
-    #when blender starts
-    #so this is only used when the add on is being re enabled in the addons panel
+
+    #import_current_or_default_json will cause an error when blender first starts
+    #this is because you cannot run the function bpy.ops.wm.save_userpref() inside save_pref()
+    #as blender cannot allow
+    #to access bpy.ops when file starts when blender just starts
+    #however, this will still load the json file to the current add on preferences for the current file
+    #it will just not load them to the default add on preferences until the user has made a change in the Save UE Shaders Panel
+    #then blender will allow access to bpy.ops and the changes will save to the default add on preferences
+    #for all files
+
+    #import_current_or_default_json here is also used when the add on 
+    #is being re enabled in the Edit > Preferences > Add Ons panel
+    #and in this case you have access to bpy.ops because blender is already open
+    #therefore, changes will happen to the current preferences and default preferences
     try:
         save_shader_map.import_current_or_default_json()
     except Exception as e:
         print("import_current_or_default_json() exception because blender is not ready yet. :", e)
+    
+    #you cannot run the function bpy.ops.wm.save_userpref() inside save_pref()
+    #as blender cannot allow
+    #to access bpy.ops when file starts when blender just starts
+    #however, this will still reset and update the json file to the current add on preferences for the current file
+    #it will just not update the presets to the default add on preferences until the user has made a change in the Save UE Shaders Panel
+    #then blender will allow access to bpy.ops and the changes will save to the default add on preferences
+    #for all files
+    try:
+        save_shader_map.reset_and_update_default_presets()
+    except Exception as e:
+        print("reset_and_update_default_presets() exception because blender is not ready yet. :", e)
 
 def unregister():
     save_shader_map.unregister()
